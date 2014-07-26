c6119545fa8204ceb300504f0952480af8d0ea3a
Ubuntu <ubuntu@ip-172-31-2-85.us-west-2.compute.internal>
2014-07-26 06:03:44 +0000

    car

---
+++ db/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = {
+	User:  mongoose.model('User', User)
+}
